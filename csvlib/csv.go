5294f8969a9369c8d720672db57e087dffd712cf
Dixon Whitmire <dixon.whitmire@coverahealth.com>
2025-05-02 22:38:38 -0400

    csvlib iterator refinements and tests

--- csvlib/csv.go
+++ csvlib/csv.go
@@ -2,6 +2,7 @@
 package csvlib
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"github.com/dixonwhitmire/golib/errorlib"
@@ -9,6 +10,80 @@ import (
 	"iter"
 	"os"
 )
+
+// DefaultBufferSize is the default buffer size used for reading records.
+const DefaultBufferSize = 1024 * 4
+
+// baseError provides the general structure for the package's exported errors.
+// The filePath and lineNumber, if applicable, provide the general context for the error.
+type baseError struct {
+	// filePath is the file system path to the csv file.
+	filePath string
+	// lineNumber is the line number where the error occurred.
+	// lineNumber == 0 for errors not related to error processing.
+	lineNumber int
+	// operation specifies the process iteration, record parsing, etc. which received an error.
+	operation string
+	// cause is the underlying exception if applicable.
+	cause error
+}
+
+// format returns the error string for the specified operation.
+// The returned error string includes the operation and file path, and optionally includes the line number and error, if
+// present.
+func (b *baseError) format(operation string) string {
+	var message string
+
+	if b.lineNumber > 0 {
+		message = fmt.Sprintf("%s error in %s line %d", operation, b.filePath, b.lineNumber)
+	} else {
+		message = fmt.Sprintf("%s error in %s", operation, b.filePath)
+	}
+
+	if b.cause != nil {
+		message += fmt.Sprintf(" %v", b.cause)
+	}
+	return message
+}
+
+// IterationError is returned when an error occurs during CSV file iteration.
+type IterationError struct {
+	*baseError
+}
+
+// Error returns context specific IterationError information.
+func (ie *IterationError) Error() string {
+	return ie.format("IterationError")
+}
+
+// Unwrap returns our inner error, aka "the cause".
+func (ie *IterationError) Unwrap() error {
+	return ie.cause
+}
+
+func NewIterationError(filePath string, lineNumber int, cause error) *IterationError {
+	return &IterationError{baseError: &baseError{filePath: filePath, lineNumber: lineNumber, cause: cause}}
+}
+
+// ConversionError is returned when an error occurs converting the underlying CSV record from []string to type T.
+type ConversionError struct {
+	*baseError
+}
+
+// Error returns context specific ConversionError information.
+func (ce *ConversionError) Error() string {
+	return ce.format("ConversionError")
+}
+
+// Unwrap returns our inner error, aka "the cause".
+func (ce *ConversionError) Unwrap() error {
+	return ce.cause
+}
+
+// NewConversionError returns a ConversionError with the specified context information.
+func NewConversionError(filePath string, lineNumber int, cause error) *ConversionError {
+	return &ConversionError{baseError: &baseError{filePath: filePath, lineNumber: lineNumber, cause: cause}}
+}
 
 // Record encapsulates a csv record including the record's line number and associated data.
 type Record[T any] struct {
@@ -19,41 +94,82 @@ type Record[T any] struct {
 // ConversionFunc is used to convert a CSV record []string to type T
 type ConversionFunc[T any] func([]string) (T, error)
 
-// Iterator returns a Seq2 iterator, returning Record[T] and an error, if applicable.
-// Data from the underlying CSV file is mapped to T using a ConversionFunc.
-func Iterator[T any](
-	inputFilePath string,
+// NewDefaultIterator returns an iterator with a DefaultBufferSize.
+func NewDefaultIterator[T any](inputFilePath string,
 	hasHeader bool,
 	conversionFunc ConversionFunc[T]) (iter.Seq2[Record[T], error], error) {
+	return iterator[T](inputFilePath, hasHeader, DefaultBufferSize, conversionFunc)
+}
+
+// NewIterator returns a buffered iterator with a configurable buffer size.
+// DefaultBufferSize is used if bufferSize < DefaultBufferSize.
+func NewIterator[T any](inputFilePath string,
+	hasHeader bool,
+	bufferSize int,
+	conversionFunc ConversionFunc[T]) (iter.Seq2[Record[T], error], error) {
+	return iterator[T](inputFilePath, hasHeader, bufferSize, conversionFunc)
+}
+
+// iterator returns iter.Seq2[Record[T], error.
+// Data from the underlying CSV file is read using a buffered csv.Reader and is mapped to T using a ConversionFunc.
+// Custom buffer sizes may be specified if customBufferSize is set to a value > DefaultBufferSize.
+func iterator[T any](
+	inputFilePath string,
+	hasHeader bool,
+	customBufferSize int,
+	conversionFunc ConversionFunc[T]) (iter.Seq2[Record[T], error], error) {
+
+	if conversionFunc == nil {
+		err := errorlib.CreateError("iterator", "conversionFunc is required")
+		return nil, NewIterationError(inputFilePath, 0, err)
+	}
 
 	f, err := os.Open(inputFilePath)
 	if err != nil {
-		return nil, errorlib.CreateError("Iterator", fmt.Sprintf("error accessing: %s %v", inputFilePath, err))
+		return nil, NewIterationError(inputFilePath, 0, err)
 	}
 
-	if conversionFunc == nil {
-		return nil, errorlib.CreateError("Iterator", "conversionFunc is required")
+	bufSize := DefaultBufferSize
+	if customBufferSize > DefaultBufferSize {
+		bufSize = customBufferSize
 	}
-
-	reader := csv.NewReader(f)
+	reader := csv.NewReader(bufio.NewReaderSize(f, bufSize))
 
 	return func(yield func(Record[T], error) bool) {
-		defer f.Close()
+		defer func() { _ = f.Close() }()
 
 		lineNumber := 0
-		for {
-			record, err := reader.Read()
-			if err == io.EOF {
-				break
-			}
+		if hasHeader {
 			lineNumber++
-			if hasHeader && lineNumber == 1 {
+			if _, err := reader.Read(); err != nil {
+				return
+			}
+		}
+
+		for {
+			lineNumber++
+			csvFields, err := reader.Read()
+			// handle csv read errors
+			if err != nil {
+				if err == io.EOF {
+					return
+				}
+				// general iteration error
+				if !yield(Record[T]{LineNumber: lineNumber}, NewIterationError(inputFilePath, lineNumber, err)) {
+					return
+				}
 				continue
 			}
-			convertedData, err := conversionFunc(record)
-			rec := Record[T]{LineNumber: lineNumber, Data: convertedData}
-			if !yield(rec, err) {
-				break
+			convertedData, err := conversionFunc(csvFields)
+			// record conversion error
+			if err != nil {
+				if !yield(Record[T]{LineNumber: lineNumber}, NewConversionError(inputFilePath, lineNumber, err)) {
+					return
+				}
+				continue
+			}
+			if !yield(Record[T]{LineNumber: lineNumber, Data: convertedData}, nil) {
+				return
 			}
 		}
 	}, nil
--- csvlib/csv_test.go
+++ csvlib/csv_test.go
@@ -1,62 +1,168 @@
 package csvlib
 
 import (
+	"errors"
 	"github.com/google/go-cmp/cmp"
-	"path"
+	"strings"
 	"testing"
 )
 
-const testDataDirectory = "./testdata"
+const (
+	sampleFilePath         = "./testdata/sample.csv"
+	sampleFileNoHeaderPath = "./testdata/sample-no-header.csv"
+	sampleFileErrorPath    = "./testdata/sample-conversion-error.csv"
+)
 
-func TestIterator_StringSlice(t *testing.T) {
-	tt := map[string]struct {
-		inputFilePath string
-		hasHeader     bool
-		expected      []Record[[]string]
-	}{
-		"has-header": {
-			inputFilePath: path.Join(testDataDirectory, "sample.csv"),
-			hasHeader:     true,
-			expected: []Record[[]string]{
-				{LineNumber: 2, Data: []string{"John", "Doe"}},
-				{LineNumber: 3, Data: []string{"Jane", "Doe"}},
-			},
-		},
-		"no-header": {
-			inputFilePath: path.Join(testDataDirectory, "sample-no-header.csv"),
-			hasHeader:     false,
-			expected: []Record[[]string]{
-				{LineNumber: 1, Data: []string{"Steve", "Doe"}},
-				{LineNumber: 2, Data: []string{"Sally", "Doe"}},
-			},
-		},
+// CustomRecord is used in test cases as a "concrete" value for our generic type parameter.
+// CustomRecord is an "exported type" so that we can use the cmp.Diff tooling without additional options.
+type CustomRecord struct {
+	FirstName string
+	LastName  string
+}
+
+// customRecordConversionFunc is used to convert csv fields to a CustomRecord for test cases.
+func customRecordConversionFunc(csvFields []string) (CustomRecord, error) {
+	customRecord := CustomRecord{
+		FirstName: csvFields[0],
+		LastName:  csvFields[1],
 	}
-
-	// conversionFunc is our pass-through conversion function which returns the csv record []string
-	conversionFunc := func(record []string) ([]string, error) {
-		return record, nil
+	if strings.EqualFold(customRecord.LastName, "error") {
+		return customRecord, NewConversionError(sampleFileErrorPath, 2, errors.New("test case error"))
 	}
+	return customRecord, nil
+}
 
-	for name, tc := range tt {
-		t.Run(name, func(t *testing.T) {
+// iteratorTestCase uses a type parameter to parameterize iterator test cases.
+type iteratorTestCase[T any] map[string]struct {
+	csvPath   string
+	hasHeader bool
+	want      []Record[T]
+	wantErr   bool
+}
 
-			got := make([]Record[[]string], 0)
-			recs, err := Iterator[[]string](tc.inputFilePath, tc.hasHeader, conversionFunc)
+// runIteratorTestCases executes the iteratorTestCase for the specified parameter type
+func runIteratorTestCases[T any](t *testing.T, conv ConversionFunc[T], cases iteratorTestCase[T]) {
+	t.Helper()
 
-			if err != nil {
-				t.Fatalf("Iterator(%q, %t) failed: %v", tc.inputFilePath, tc.hasHeader, err)
+	for name, tt := range cases {
+		t.Run(name, func(subT *testing.T) {
+			got := make([]Record[T], 0)
+			iter, err := NewDefaultIterator[T](tt.csvPath, tt.hasHeader, conv)
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("iterator error = %v, wantErr %v", err, tt.wantErr)
 			}
-
-			for rec, err := range recs {
+			for rec, err := range iter {
 				if err != nil {
-					t.Fatalf("Iterator(%q, %t) failed: %v", tc.inputFilePath, tc.hasHeader, err)
+					t.Errorf("iterator error = %v", err)
 				}
 				got = append(got, rec)
 			}
-
-			if diff := cmp.Diff(tc.expected, got); diff != "" {
-				t.Errorf("Iterator(%q, %t) found diff (-want +got):\n%s", tc.inputFilePath, tc.hasHeader, diff)
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Errorf("iterator found diff (-want +got):\n%s", diff)
 			}
 		})
+	}
+}
+
+func TestIterator_StringSlice(t *testing.T) {
+	tt := iteratorTestCase[[]string]{
+		"has-header": {
+			csvPath:   sampleFilePath,
+			hasHeader: true,
+			want: []Record[[]string]{
+				{LineNumber: 2, Data: []string{"John", "Doe"}},
+				{LineNumber: 3, Data: []string{"Jane", "Doe"}},
+			},
+			wantErr: false,
+		},
+		"has-no-header": {
+			csvPath:   sampleFileNoHeaderPath,
+			hasHeader: false,
+			want: []Record[[]string]{
+				{LineNumber: 1, Data: []string{"Steve", "Doe"}},
+				{LineNumber: 2, Data: []string{"Sally", "Doe"}},
+			},
+			wantErr: false,
+		},
+	}
+
+	conversionFunc := func(records []string) ([]string, error) {
+		return records, nil
+	}
+
+	runIteratorTestCases[[]string](t, conversionFunc, tt)
+}
+
+func TestIterator_CustomType(t *testing.T) {
+	tt := iteratorTestCase[CustomRecord]{
+		"has-header": {
+			csvPath:   sampleFilePath,
+			hasHeader: true,
+			want: []Record[CustomRecord]{
+				{
+					LineNumber: 2,
+					Data: CustomRecord{
+						FirstName: "John",
+						LastName:  "Doe",
+					},
+				},
+				{
+					LineNumber: 3,
+					Data: CustomRecord{
+						FirstName: "Jane",
+						LastName:  "Doe",
+					},
+				},
+			},
+			wantErr: false,
+		},
+		"has-no-header": {
+			csvPath:   sampleFileNoHeaderPath,
+			hasHeader: false,
+			want: []Record[CustomRecord]{
+				{
+					LineNumber: 1,
+					Data: CustomRecord{
+						FirstName: "Steve",
+						LastName:  "Doe",
+					},
+				},
+				{
+					LineNumber: 2,
+					Data: CustomRecord{
+						FirstName: "Sally",
+						LastName:  "Doe",
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+	runIteratorTestCases[CustomRecord](t, customRecordConversionFunc, tt)
+}
+
+func TestIterator_IterationError(t *testing.T) {
+	_, err := NewDefaultIterator[CustomRecord]("/tmp/not-a-real.csv", true, customRecordConversionFunc)
+	if err == nil {
+		t.Fatal("NewDefaultIterator did not return an IterationError")
+	} else {
+		var ie *IterationError
+		if !errors.As(err, &ie) {
+			t.Errorf("NewDefaultIterator did not return an IterationError got %v", err)
+		}
+	}
+}
+
+func TestIterator_ConversionError(t *testing.T) {
+	iter, err := NewDefaultIterator[CustomRecord](sampleFileErrorPath, true, customRecordConversionFunc)
+	if err != nil {
+		t.Fatalf("NewDefaultIterator unexpected error %v", err)
+	}
+
+	var ce *ConversionError
+	for _, err := range iter {
+		if !errors.As(err, &ce) {
+			t.Errorf("NewDefaultIterator did not return an ConversionError got %v", err)
+		}
 	}
 }
